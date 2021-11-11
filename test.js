8e962e68f35215921d18dbab35f04279de9f2728
balaguru4580 <balaguru4580@gmail.com>
2021-11-11 17:29:18 +0530

    Test 4

--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Hello Pondicherry"); // changed to pondicherry
-=======
-console.log("Hello Coloured Cow");
->>>>>>> c580da36bfc278bc355eac85d3c95de34c071b5e
+console.log("Hello Pondicherry");
